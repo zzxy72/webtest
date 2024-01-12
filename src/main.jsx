25983732226a744912154ddd859729969d98d2a0
zzxy72 <keytest@naver.com>
2024-01-12 09:26:11 +0900

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
+import "./bootstrap/dist/css/bootstrap.css";
 import Game from "./Game";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Game />} />
+      <Route path="./" element={<Game />} />
     </Routes>
   </BrowserRouter>
 );
